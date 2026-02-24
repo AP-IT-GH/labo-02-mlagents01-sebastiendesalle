de36b045c43df289f421ebb05c89866646976daa
sebastien <sebastien.desalle@student.ap.be>
2026-02-24 17:31:05 +0100

    Finalize agent with eyes

---
+++ Assets/Scripts/CubeAgentRays.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class CubeAgentRays : Agent
+{
+    public Transform Target;
+    public float speedMultiplier = 0.1f;
+    public float rotationMultiplier = 5f;
+    public override void OnEpisodeBegin()
+    {
+        if (this.transform.localPosition.y < 0)
+        {
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+            this.transform.localRotation = Quaternion.identity;
+        }
+        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(this.transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.z = actionBuffers.ContinuousActions[0];
+        transform.Translate(controlSignal * speedMultiplier);
+
+        transform.Rotate(0.0f, rotationMultiplier * actionBuffers.ContinuousActions[1], 0.0f);
+
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        else if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+}
