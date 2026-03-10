f45948d8827047b5324b159ed201e6ffa9ba72fe
sebastien <sebastien.desalle@student.ap.be>
2026-03-10 15:28:01 +0100

    Add two stage agent with ray casting

--- Assets/Scripts/CubeAgentTwoStage.cs
+++ Assets/Scripts/CubeAgentTwoStage.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using Unity.VisualScripting;
 
 public class CubeAgentTwoStage : Agent
 {
     public Transform Target;
-    public Transform GreenZone;
 
     private bool targetCollected;
     public float speedMultiplier = 0.5f;
+    public float rotationMultiplier = 5.0f;
 
     public override void OnEpisodeBegin()
     {
@@ -30,6 +31,44 @@ public class CubeAgentTwoStage : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
+        sensor.AddObservation(targetCollected ? 1.0f : 0.0f);
+    }
 
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.z = actionBuffers.ContinuousActions[0];
+        transform.Translate(controlSignal * speedMultiplier);
+        transform.Rotate(0.0f, rotationMultiplier * actionBuffers.ContinuousActions[1], 0.0f);
+
+        AddReward(-1f / MaxStep);
+
+        if (this.transform.localPosition.y < 0)
+        {
+            SetReward(-1.0f);
+            EndEpisode();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Target") && !targetCollected)
+        {
+            targetCollected = true;
+            Target.gameObject.SetActive(false);
+            AddReward(0.5f);
+        }
+        else if (other.CompareTag("Green Zone") && targetCollected)
+        {
+            AddReward(1.0f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Vertical");
+        continuousActionsOut[1] = Input.GetAxis("Horizontal");
     }
 }
